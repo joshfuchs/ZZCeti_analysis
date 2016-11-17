3e163449eb36a7cf282929c05d210f60c320e488
Josh Fuchs <fuchs.josh@gmail.com>
2016-11-17 15:57:51 -0500

    Option to print info given conditions and file renamed

--- logg_teff_plot.py
+++ logg_teff_plot.py
@@ -31,6 +31,10 @@ for m in np.arange(len(arr)):
 
 logg = logg / 1000.
 
+#Print solutions that meet particular value
+for x in np.arange(len(Teff)):
+    if Teff[x] == 15000. or Teff[x] == 10000. or logg[x] == 9.5 or logg[x] == 7.0:
+        print Teff[x], logg[x], blue_file[x]
 plt.clf()
 plt.plot(Teff,logg,'bo')
 plt.xlim(15500,9500)
@@ -38,5 +42,18 @@ plt.ylim(9.75,7.0)
 #Plot Gianninas 2011 instability strip
 plt.plot([14000.,11600.],[8.82,7.36],'r')
 plt.plot([11800,10600],[8.88,7.35],'r')
+#Plot 4 horsemen
+#plt.plot(12740,8.0,'ro')
+#plt.plot(12790,8.0,'ro')
+#plt.plot(12780,8.05,'ro')
+#plt.plot(12790,8.05,'ro')
+#plot ensemble from Chris
+plt.plot(12300,8.05,'ro') #SDSS J1151+0525, EPIC 201802933
+plt.plot(14290,8.50,'ro') #SDSS J0837+1856, EPIC 211914185 low S/N?
+plt.plot(12790,8.05,'ro') #L19-2 from 2015-05-22
+plt.plot(12790,8.00,'ro') #GD 165 from 2015-04-26
+plt.plot(12480,7.94,'ro') #R548 from 2015-08-23
+plt.plot(11830,7.95,'ro') #SDSS J0840+1303, EPIC 228682478 from 2016-01-07
+plt.plot(12610,8.00,'ro') #SDSS J0051+0339, EPIC 220347759
 plt.show()
 
